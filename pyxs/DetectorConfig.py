d2cdcc5573aeed9fb258c9dca0b52ceace3bbd05
Lin Yang <lyang@bnl.gov>
2017-04-04 16:05:02 -0400

    added more parameters common for data processing

--- pyxs/DetectorConfig.py
+++ pyxs/DetectorConfig.py
@@ -1,9 +1,14 @@
 
 
 class DetectorConfig():
-    def __init__(self, extension = "", dark = None, flat = None, dezinger = False):
+    def __init__(self, extension = "", dark = None, flat = None, dezinger = False, exp_para = None, qgrid = None, mask = "",
+                 fix_scale=None):
         self.dark = dark
         self.flat = flat
         self.dezinger = dezinger
         self.extension = extension
+        self.exp_para = exp_para
+        self.qgrid = qgrid
+        self.mask = mask
+        self.fix_scale = fix_scale
 
